d1fd51b49c6896e0f04c36fb75963470bc5a3731
Waliston <goneisgone@hotmail.com>
2023-06-10 11:36:43 -0300

    React Native - Examples, Components no mesmo arquivo

--- examples/App.js
+++ examples/App.js
@@ -1,19 +1,13 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { Image, Text, View } from "react-native";
+import { StyleSheet, Image, Text, View, Button } from "react-native";
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View>
-        <Text>Minha Imagem</Text>
-        <Image
-          style={{ width: 150, height: 150 }}
-          source={{
-            uri: "https://storage.ko-fi.com/cdn/useruploads/display/39d7fa2f-9353-4cb1-91f9-7c5c1c2d4f61_0f217d0189f841ae794500966ab1845a.jpg",
-          }}
-        />
-      </View>
-    );
-  }
+const App = () => {
+  return (
+    <View style={{ marginTop: 20}}>
+      <Text style={{textAlign: "center"}}>Oi</Text>
+      <Button title="Clique"></Button>
+    </View>
+  );
 }
+
+export default App;
