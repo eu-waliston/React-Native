2f316cdeb90981e48a9bc38f4c1d14319548c25c
Waliston <goneisgone@hotmail.com>
2023-06-14 11:12:29 -0300

    ocultando a yellow box

--- examples/App.js
+++ examples/App.js
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import Body from "./components/Body";
 
 const App = () => {
+  console.disableYellowBox = false;
 
   return (
     <ScrollView style={styles.container}>
