e0a300af005d4da00da3256ec3d2cf11df23d551
Waliston <goneisgone@hotmail.com>
2023-06-24 09:46:31 -0300

    React Native - Logica I

--- logica/components/Header.jsx
+++ logica/components/Header.jsx
@@ -1,28 +1,31 @@
 import React from "react";
-import { Text, View,StyleSheet } from "react-native";
-
+import { Text, View, StyleSheet } from "react-native";
 
 const Header = () => {
+  const name = "Waliston";
+  var idade = 27;
+  let cidade = "SM";
+
   return (
     <View style={header.bar}>
-      <Text style={header.text}>LOGICA</Text>
-    </View>   
-  )
-}
+      <Text style={header.text}>Olá, {name}</Text>
+    </View>
+  );
+};
 
 const header = StyleSheet.create({
   bar: {
     backgroundColor: "#0E2954",
     marginTop: 25,
-    width: '100%',
+    width: "100%",
     flex: 0.07,
   },
   text: {
     textAlign: "center",
     marginTop: 15,
     fontWeight: "bold",
-    color: "#fff"
-  }
-})
+    color: "#fff",
+  },
+});
 
 export default Header;
